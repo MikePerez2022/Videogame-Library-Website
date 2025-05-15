83e3cbdd77d7863dca95d22b10a9c46886392fd9
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-05-15 13:05:28 -0600

    Revert "started factories"
    This reverts commit 9d797747f22b5371c09ab6d6d8218a6e412ef924.

--- Factories/VideoGameFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using VideoGameLibrary.Models;
-
-namespace VideoGameLibrary.Factories
-{
-    public abstract class VideoGameFactory
-    {
-        public abstract VideoGame CreateGame(string title, int year, string platform, string genre, string esrb, string image);
-    }
-}
